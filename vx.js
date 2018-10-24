ef34856c314674f82b6b34ba777a9ae1844030a6
CollierPlays <collier.bell20@gmail.com>
2018-10-23 20:45:57 -0500

    Update vx.js

--- vx.js
+++ vx.js
@@ -12,13 +12,13 @@ var prefix = "v";
 var token = " ";
 
 client.on("ready", () => {
-  console.log("Vulnix | Logged in! Server count: ${client.guilds.size}");
-  client.user.setGame(`vhelp / vnew | ${client.guilds.size} servers`);
+  console.log("Tickets | Logged in! Server count: ${client.guilds.size}");
+  client.user.setGame(`on LiteAlts`);
 });
 
 client.on("guildCreate", (guild) => {
-client.user.setGame(`vhelp / vnew | ${client.guilds.size} servers`);
-    guild.owner.user.send(`Hello! I'm Vulnix!\nThanks for adding me to your guild!\n\nView all of my commands with \`vhelp\`.\nLearn more about me with \`vabout\`.\n\n**About:**Vulnix is a simple Discord support ticket bot that aims to provide easy to use mod and support functions for all servers!\n\n\Enjoy! ~Vulnix Teamn\*Need help? Wanna chill?*  Join the Vulnix Discord! https://discord.gg/HqNPFTC`);
+client.user.setGame(`on LiteAlts`);
+    guild.owner.user.send(`Time to kick out tickety!`);
 });
 
 client.on("message", (message) => {
@@ -26,26 +26,26 @@ client.on("message", (message) => {
 
   if (message.content.toLowerCase().startsWith(prefix + `help`)) {
     const embed = new Discord.RichEmbed()
-    .setTitle(`:mailbox_with_mail: Vulnix Help`)
+    .setTitle(`Is it a bird? A plane? no.. its Collier's tickets!`)
     .setColor(0xCF40FA)
-    .setDescription(`Hello! I'm Vulnix, the Discord bot for super cool support ticket stuff and more! Here are my commands:`)
-    .addField(`Tickets`, `[${prefix}new]() > Opens up a new ticket and tags the Support Team\n[${prefix}close]() > Closes a ticket that has been resolved or been opened by accident`)
-    .addField(`Other`, `[${prefix}help]() > Shows you this help menu your reading\n[${prefix}ping]() > Pings the bot to see how long it takes to react\n[${prefix}about]() > Tells you all about Vulnix`)
+    .setDescription(``)
+    .addField(`Tickets`, `[${prefix}new]() > Opens up a new ticket\n[${prefix}close]() > Closes a ticket.`)
+    .addField(`Other`, `[${prefix}help]() > Shows you this help menu\n[${prefix}ping]() > Pings the bot to see how long it takes to respond.`)
     message.channel.send({ embed: embed });
   }
 
   if (message.content.toLowerCase().startsWith(prefix + `ping`)) {
-    message.channel.send(`Hoold on!`).then(m => {
-    m.edit(`:ping_pong: Wew, made it over the ~waves~ ! **Pong!**\nMessage edit time is ` + (m.createdTimestamp - message.createdTimestamp) + `ms, Discord API heartbeat is ` + Math.round(client.ping) + `ms.`);
+    message.channel.send(`One moment!`).then(m => {
+    m.edit(`**Pong!**\ Math.round(client.ping) + `ms.`);
     });
 }
 
 if (message.content.toLowerCase().startsWith(prefix + `new`)) {
     const reason = message.content.split(" ").slice(1).join(" ");
-    if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
-    if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`You already have a ticket open.`);
+    if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a `Staff` role made, so the ticket won't be opened.`);
+    if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`Sorry, you already have a ticket open.`);
     message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
-        let role = message.guild.roles.find("name", "Support Team");
+        let role = message.guild.roles.find("name", "Staff");
         let role2 = message.guild.roles.find("name", "@everyone");
         c.overwritePermissions(role, {
             SEND_MESSAGES: true,
@@ -59,18 +59,18 @@ if (message.content.toLowerCase().startsWith(prefix + `new`)) {
             SEND_MESSAGES: true,
             READ_MESSAGES: true
         });
-        message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
+        message.channel.send(`:white_check_mark: Ok, i made your ticket!, #${c.name}.`);
         const embed = new Discord.RichEmbed()
         .setColor(0xCF40FA)
-        .addField(`Hey ${message.author.username}!`, `Please try explain why you opened this ticket with as much detail as possible. Our **Support Team** will be here soon to help.`)
+        .addField(`Hey ${message.author.username}!`, `Thanks for contacting us. We will be here to help you shortly!`)
         .setTimestamp();
         c.send({ embed: embed });
     }).catch(console.error);
 }
 if (message.content.toLowerCase().startsWith(prefix + `close`)) {
-    if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
+    if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`Sorry, You can't use this outside of a ticket channel!`);
 
-    message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`-confirm\`. This will time out in 10 seconds and be cancelled.`)
+    message.channel.send(`Ok. Please type this command again to close the ticket. Else, this request will expire in 10 seconds.`)
     .then((m) => {
       message.channel.awaitMessages(response => response.content === '-confirm', {
         max: 1,
@@ -81,7 +81,7 @@ if (message.content.toLowerCase().startsWith(prefix + `close`)) {
           message.channel.delete();
         })
         .catch(() => {
-          m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
+          m.edit('Timed out, ticket not closed.').then(m2 => {
               m2.delete();
           }, 3000);
         });
